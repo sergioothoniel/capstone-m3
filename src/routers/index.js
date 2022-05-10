5fecfc3097c68e5dc2d86c8476c3349d42cfb7f6
sergioothoniel <sergioothoniel@hotmail.com>
2022-05-10 14:24:04 -0300

    fix

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom"
 const Routers = () =>{
     return(
         <Switch>
-            <Route>Teste</Route>
+            <Route>Teste certo</Route>
         </Switch>
     )
 }
