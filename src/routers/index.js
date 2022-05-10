3552ad04b7cf25745928230db82354ad3ce2e4d6
sergioothoniel <sergioothoniel@hotmail.com>
2022-05-10 14:17:02 -0300

    create: rotas

---
+++ src/routers/index.js
@@ -0,0 +1,11 @@
+import { Switch, Route } from "react-router-dom"
+
+const Routers = () =>{
+    return(
+        <Switch>
+            <Route></Route>
+        </Switch>
+    )
+}
+
+export default Routers
