a852b21453351337ce39890653ba809496b49e8c
marianabonan <marianabonanbs@gmail.com>
2022-05-12 14:02:19 -0300

    fix: correção de padrões

--- src/App.js
+++ src/App.js
@@ -8,22 +8,14 @@ import {
   Code,
   Grid,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
 import { theme } from './styles/theme';
-import Input from './components/Input';
+import Routers from './routers';
 
 function App() {
   return (
     <ChakraProvider theme={theme} resetCSS={true}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <Input />
-          <Input isPassword />
-          <VStack spacing={8}></VStack>
-        </Grid>
-      </Box>
+      <Routers />
     </ChakraProvider>
   );
 }
--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,15 +1,22 @@
-import { Flex, Select } from "@chakra-ui/react";
+import { Flex, Select as SelectChakra } from '@chakra-ui/react';
 
-
-
-const StyledSelect = ({width, height, children, placeholder}) => {
-    
-    return (
-        <Flex>
-            <Select variant='outline' fontSize="1.5 rem" color="gray.700" border="1px solid" borderColor="primary.0" focusBorderColor="primary.0" placeholder={placeholder} width={width} height={height}>
-        	{children}
-            </Select>
-        </Flex>
-        )
-  };
-  export default StyledSelect;
+const Select = ({ width, height, children, placeholder }) => {
+  return (
+    <Flex>
+      <SelectChakra
+        variant="outline"
+        fontSize="1.5 rem"
+        color="gray.700"
+        border="1px solid"
+        borderColor="primary.0"
+        focusBorderColor="primary.0"
+        placeholder={placeholder}
+        width={width}
+        height={height}
+      >
+        {children}
+      </SelectChakra>
+    </Flex>
+  );
+};
+export default Select;
