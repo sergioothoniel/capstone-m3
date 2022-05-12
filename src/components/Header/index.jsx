fddd631e21d79184bbb8402b0cde1e148cd3f3c3
Isaac Xavier <ayzick120@gmail.com>
2022-05-12 16:07:45 -0300

    create: componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,40 @@
+import { Flex, Grid, GridItem } from '@chakra-ui/react';
+
+function Header({ childrenLeft, childrenMiddle, childrenRight }) {
+  return (
+    <Flex
+      bg="primary.100"
+      w="100vw"
+      h={['106px', '106px', '127px']}
+      as="header"
+      px={['40px', '40px', '54px']}
+      align="center"
+      justify="center"
+    >
+      <Grid
+        h="100%"
+        w="100%"
+        maxW="1160px"
+        templateColumns="repeat(3, 1fr)"
+        alignItems="center"
+        justify="center"
+      >
+        <GridItem
+          display="flex"
+          alignItems="center"
+          justifyContent="flex-start"
+        >
+          {childrenLeft}
+        </GridItem>
+        <GridItem display="flex" alignItems="center" justifyContent="center">
+          {childrenMiddle}
+        </GridItem>
+        <GridItem display="flex" alignItems="center" justifyContent="center">
+          {childrenRight}
+        </GridItem>
+      </Grid>
+    </Flex>
+  );
+}
+
+export default Header;
