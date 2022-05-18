6151550d97147e3d69e869511062ed186938695d
sergioothoniel <sergioothoniel@hotmail.com>
2022-05-18 16:10:57 -0300

    update: agenda de horarios

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,52 +1,71 @@
 import { TableContainer } from "./styles"
 import { useSchedules } from "../../Providers/schedules"
 import { useDataUser } from "../../Providers/dataUser"
+import { useState, useEffect } from "react"
 
 const Calendar = () =>{
-
+    
     const {schedules} = useSchedules()
     const {dataUser} = useDataUser()
 
     const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
     const schedulesDefeault= ['09:00', '10:00', '11:00','13:00','14:00', '15:00', '16:00']
+    const daysOfWeek = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
 
     const today = new Date()
     const day = today.getDate()
-    const month = months[today.getMonth()]  
-    
-    const nesxtSevenDays = ()=>{
+      
+       
+    const currentWeek = (week = 0)=>{
         let arr = []
+        const firstDay = day+7*week
 
-        for(let i = day; i<day+7; i++){
-            arr.push(i)
+        for(let i = firstDay; i<firstDay+7; i++){
+            const newDate = new Date(2022, today.getMonth(), i)
+            arr.push(newDate)
         }
-        return(arr)
+        return arr
     }
-    
-        
+           
     
 
     return(
        <TableContainer>
-           <h1>{month}</h1>
+           
+           <h1>2022</h1>
            <div className="table">
-               {nesxtSevenDays().map(day=>{
-                   const date = `${day}/${today.getMonth()+1}/${today.getFullYear()}`
-                   const appointmentsToday = schedules.filter(appointment => appointment.date === date).filter(appointment => appointment.staffId === dataUser.id.toString())
-                   
+               {currentWeek().map(date=>{
+                   const dateFormated = date.toLocaleDateString('pt-BR')
+                   const appointmentsToday = schedules.filter(appointment => appointment.date === dateFormated).filter(appointment => appointment.staffId === dataUser.id.toString())
+                   const currentDay = dateFormated.slice(0, -5)
+                   const dayOfWeek = daysOfWeek[date.getDay()]
+                 
+
                    return(
                    <div className="table-column">
-                       <span>{day}</span>
+                       <span>{currentDay}</span>
+                       <span>{dayOfWeek}</span>
                        {schedulesDefeault.map(horario=>{
                            const appointment =  appointmentsToday.length > 0 ? appointmentsToday.find(appointment => appointment.time === horario) : false
-                           const scheduleCheck = !!appointment 
-                           console.log(scheduleCheck)
-                        return(
-                          <span><button id={scheduleCheck && 'confirmed'}>{horario}</button></span> 
-                       )})}
+                           const scheduleCheck = !!appointment    
+                           
+                           if(dayOfWeek === 'Sab' || dayOfWeek === 'Dom'){
+                               return(
+                                   <span id='fds'>
 
-                   </div>
-                   
+                                   </span>
+                               )
+                           }else{
+                               return(
+                                <span><button id={scheduleCheck && 'confirmed'} onClick={()=>console.log(appointment)}>{horario}</button></span>
+
+                               )
+                           }                   
+                                                     
+                                                 
+                       })}
+
+                   </div>                   
                )}
                )}
            </div>
